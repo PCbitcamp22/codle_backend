41c43ebc34a2ff670effaf69b69ed5c0dfaf2251
Sam Galita <samrgmtg@gmail.com>
2022-04-09 16:05:38 -0400

    Co-authored-by: daniel03chung <daniel03chung@users.noreply.github.com>

--- Controller.py
+++ Controller.py
@@ -3,21 +3,29 @@ from datetime import date
 
 
 #We get today's date
-date_time = date.today().strftime("%m/%d/%Y")
-questionOfTheDay = backend.questionList[date_time]
+
+#date_time = date.today().strftime("%m/%d/%Y")
+#questionOfTheDay = backend.questionList[date_time]
 
 
 # send them question
-questionTextToSend = questionOfTheDay.getText()
+#questionTextToSend = questionOfTheDay.getText()
+
+def getTodayProblem():
+    date_time = date.today().strftime("%m/%d/%Y")
+    questionOfTheDay = backend.questionList[date_time]
+    questionTextToSend = questionOfTheDay.getText()
+    return questionTextToSend
 
 # get input code
-inputCodeFromServer: str = ("""def addNums(num1, num2):
-    return nums + num2""") # get code with api
+#inputCodeFromServer: str = ("""def addN ums(num1, num2):
+#    return num1 + num2""") # get code with api
 
 # send them results
 
-#exec(inputCodeFromServer, globals())
-results = questionOfTheDay.getResultsWithInput(inputCodeFromServer)
-print(results)
+def getResults(inputCodeFromServer: str):
+    date_time = date.today().strftime("%m/%d/%Y")
+    questionOfTheDay = backend.questionList[date_time]
+    return questionOfTheDay.getResultsWithInput(inputCodeFromServer)
 
 
