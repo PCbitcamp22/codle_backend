74b8186641ac716d1c560a4282b38edc0445ed52
Sam Galita <samrgmtg@gmail.com>
2022-04-09 12:31:56 -0400

    Co-authored-by: daniel03chung <daniel03chung@users.noreply.github.com>

--- questions.py
+++ questions.py
@@ -1,10 +1,10 @@
 import backend as bk
 
 def q1_t1():
-    addNums(5, 8)
+    return addNums(5, 8)
 
 def q1_t2():
-    addNums(6, 9)
+    return addNums(6, 9)
 
 def q1_t3():
     return addNums(-100, 5)
@@ -21,10 +21,19 @@ def q1_t6():
 q1 = bk.question("How can we add two numbers?", [13, 15, -95, 135, 194, 0], q1_t1, q1_t2, q1_t3, q1_t4, q1_t5, q1_t6)
 
 
-inputString = """def addNums(num1, num2):
+sample_string = """def addNums(num1, num2):
     return num1 + num2"""
 
-print(q1.getResultsWithInput(inputString))
+#print(q1.getResultsWithInput(sample_string))
+
+exec(sample_string, globals())
+
+print(addNums(0,5))
+
+print(q1_t3())
+
+
+
 
 
 
